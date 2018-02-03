10c9c7db34529c7475af45987cb9832bb75b16bf
Ted Lorts <tplorts@gmail.com>
2018-02-02 21:42:56 -0800

    no longer using these shell scripts to upload; using s3-deploy now

--- upload-districts.sh
+++
@@ -1,5 +0,0 @@
-USA_DATA_BASE_DIR='dist'
-S3_DATA_BUCKET='dev-data.mapmyreps.us'
-FOLDER=geography/districts
-
-# aws s3 cp $USA_DATA_BASE_DIR/$FOLDER s3://$S3_DATA_BUCKET/$FOLDER --recursive --acl public-read
