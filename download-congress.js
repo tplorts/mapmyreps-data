067525f32af7fd0166abb45fd8f6c6c3a0070cbd
Ted Lorts <tplorts@gmail.com>
2018-08-24 15:57:11 -0700

    format with standardjs

--- download-congress.js
+++ download-congress.js
@@ -3,9 +3,7 @@ const request = require('request-promise-native')
 
 const congressDir = 'dist/congress'
 
-
-
-function sourceUrl(filename) {
+function sourceUrl (filename) {
   return `https://theunitedstates.io/congress-legislators/${filename}`
 }
 
@@ -13,22 +11,21 @@ const DataFiles = [
   'legislators-current',
   'committees-current',
   'committee-membership-current',
-  'legislators-social-media',
+  'legislators-social-media'
 ].map(x => `${x}.json`)
 
-async function get(filename) {
+async function get (filename) {
   return request({ url: sourceUrl(filename) })
 }
 
-
-async function saveCondensed(filename) {
+async function fetchAndSaveCondensed (filename) {
   const sourceJson = await get(filename)
   const condensedJson = JSON.stringify(JSON.parse(sourceJson))
   const path = `${congressDir}/${filename}`
   return new Promise((resolve, reject) => {
-    fs.writeFile(path, condensedJson, err => { if (err) { reject(err) } else { resolve() } })
+    fs.writeFile(path, condensedJson, err => (err ? reject(err) : resolve()))
   })
 }
 
-const writeAll = Promise.all(DataFiles.map(f => saveCondensed(f)))
-writeAll.then(() => console.log('Done')).catch(err => console.error(err))
+const writeAll = Promise.all(DataFiles.map(fetchAndSaveCondensed))
+writeAll.then(() => console.log('Done')).catch(console.error)
